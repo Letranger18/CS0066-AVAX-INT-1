9cbb9a11ef1922625f474b56c3873c2c7632e491
L'etranger <92956390+Letranger18@users.noreply.github.com>
2024-12-01 01:02:02 +0800

    Add files via upload

---
+++ JBasket.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract JBasket{
+
+   // Public Variables
+   uint public constant maxCapacity = 10;
+   uint public basket = 0;
+
+   // Function to ADD egg into the basket
+   function pickUpEgg() public {
+      basket += 1;
+
+      // If the basket reached its max capacity
+      if(basket > maxCapacity){
+         revert("Basket is full, the egg was tossed");
+      }
+   }
+
+   // Function to REMOVE n egg(s) from the basket
+   function tossEgg(uint _value) public {
+
+      if(basket >= _value){   // If the eggs in the basket is more than the amount that will get tossed
+         require(_value > 0,"You cannot toss 0 egg"); // amount to toss cannot be ZERO
+         basket -= _value;
+      }
+      else if(basket <= _value){  // If the eggs in the basket is less than the amount that will get tossed
+         revert("That's too much egg!");
+      }
+      else{    // If there are no eggs in the basket
+         revert("There are no egg to toss");
+      }
+   }
+
+   // Function to test for Internal Problems
+   function checkBasket() public view {
+      assert(basket > 0 && basket <= maxCapacity );
+   }
+}
